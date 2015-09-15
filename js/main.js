d02e9504a80f3cc100a5ca2e39f2d052a6cb3a82
sergiopalooza <sergioflores836@gmail.com>
2015-09-15 17:42:51 -0500

    added new fortune button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     
     var $messages = $('#messages');
-    var skip=Math.floor(Math.random()*110)    
+    var skip=Math.floor(Math.random()*110);    
     console.log(skip); //getting a random number to skip in the list of fortunes
     
     $.ajax({ //using ajax, passing an object
@@ -9,7 +9,20 @@ $(document).ready(function(){
     	url: 'http://fortunecookieapi.com/v1/fortunes?limit=1&skip=' + skip,
     	success: function(data){
     		//console.log("success", data);
-            $messages.append('<p>' + data[0].message + '..in bed.' + '</p>');
+            $messages.text(data[0].message + '..in bed.');
     	}
     });
+
+    $('#refresh').click(function(){
+        //alert('hey!');
+        skip=Math.floor(Math.random()*110);
+        $.ajax({ //using ajax, passing an object
+        type: 'GET',
+        url: 'http://fortunecookieapi.com/v1/fortunes?limit=1&skip=' + skip,
+        success: function(data){
+            //console.log("success", data);
+            $messages.text(data[0].message + '..in bed.');
+        }
+    });
+    })
 });
